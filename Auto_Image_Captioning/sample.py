cf804a75f9a77acdf8f51e440a5bc2dd734a9e78
Dylan Lasher <38015814+DLasher95@users.noreply.github.com>
2020-03-17 01:04:24 -0600

    Implemented color-extraction
    Implemented functionality to extract any amount of dominant colors from an uploaded image. Options to display a histogram breakdown.

--- Auto_Image_Captioning/sample.py
+++ Auto_Image_Captioning/sample.py
@@ -8,6 +8,8 @@ from torchvision import transforms
 from building_vocab import Vocabulary
 from model import EncoderCNN, DecoderRNN
 from PIL import Image
+from extract_colors import DominantColors
+
 """
 Author: Dylan Lasher
 Specs: Python 3.7, Windows 10
@@ -56,29 +58,49 @@ def main(args):
     feature = encoder(image_tensor)
     sampled_ids = decoder.sample(feature)
     sampled_ids = sampled_ids[0].cpu().numpy()
+
+    # Run color analysis
+    clusters = int(args.colors)
+
+    # Number of dominant colors you will find.
+    dc = DominantColors(args.image, clusters)
+    colors = dc.dominantColors()
     
     # Convert word_ids to words
     sampled_caption = []
     for word_id in sampled_ids:
         word = vocab.idx2word[word_id]
-        sampled_caption.append(word)
         if word == '<end>':
+            # Add mood description.
+            sampled_caption += ""
+
+            sampled_caption.append(word)
             break
+        sampled_caption.append(word)
     sentence = ' '.join(sampled_caption)
     
     # Print image and generated caption
     print (sentence)
+    print(colors)
     image = Image.open(args.image)
     plt.imshow(np.asarray(image))
 
+    # Plot histogram
+    dc.plotHistogram()
+    plt.savefig("myHistogram.png")
+
 """
 To run, go to Terminal and type:
-  python sample.py --image picture_location
+  python sample.py --image picture_location --colors num_of_colors
   (where picture_location is the image. Example: images/ball.jpg)
+  (num_of_colors is the number of dominant colors you want. Example: 4)
+  
+  Check myHistogram.png for the histogram of extracted colors
 """
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--image', type=str, required=True, help='input image for generating caption')
+    parser.add_argument('--colors', type=str, required=True, help='number of dominant colors')
     parser.add_argument('--encoder_path', type=str, default='models/encoder-5-3000.pkl', help='path for trained encoder')
     parser.add_argument('--decoder_path', type=str, default='models/decoder-5-3000.pkl', help='path for trained decoder')
     parser.add_argument('--vocab_path', type=str, default='data/vocab.pkl', help='path for vocabulary wrapper')
